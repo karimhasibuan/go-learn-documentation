f3c778a6e72ed157e905940f0f652c0be2b96153
Karim Hasibuan <amfibicoding19@gmail.com>
2023-03-30 22:11:40 +0700

    refactor: create a db connetction in go

--- accessing-a-relational-database/data-access/main.go
+++ accessing-a-relational-database/data-access/main.go
@@ -1,3 +1,30 @@
 package main
 
 import "github.com/go-sql-driver/mysql"
+
+var db *sql.DB
+
+func main() {
+	// Capture connection properties.
+	cfg := mysql.Config{
+		User: os.Getenv("DBUSER"),
+		Passwd: os.Getenv("DBPASS"),
+		Net: "tcp",
+		Addr: "127.0.0.1:3306",
+		DBName: recordings,
+	}
+
+	// Get a database handle.
+	var err error
+	db, err = sql.Open("mysql", cfg.FormatDSN())
+	if err != nil {
+		log.Fatal(err)
+	}
+	
+	// Test the connection to the database
+	pingErr := db.Ping()
+	if pingErr != nil {
+		log.Fatal(pingErr)
+	}
+	fmt.Println("Successfully connected to the database!")
+}
