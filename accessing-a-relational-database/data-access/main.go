f035941656f26e50bf1bcc1d3d22ff2259baf1a6
Karim Hasibuan <amfibicoding19@gmail.com>
2023-03-30 22:38:03 +0700

    feat: make a albumsByartist function for show artist data from database

--- accessing-a-relational-database/data-access/main.go
+++ accessing-a-relational-database/data-access/main.go
@@ -40,4 +40,36 @@ func main() {
 		log.Fatal(pingErr)
 	}
 	fmt.Println("Successfully connected to the database!")
+
+	// Call the albumsByArtist function and print the results.
+	albums, err := albumsByartist("John Coltrane")
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("%d albums by John Coltrane\n", len(albums))
+}
+
+// albumsByartist queries for albums that have the specified artist name.
+func albumsByartist(name string) ([]Album, error) {
+	// An albums slice to hold data from returned rows.
+	var albums []Album
+
+	rows, err := db.Query("SELECT * FROM albums WHERE artist = ?", name)
+	if err != nil {
+		return nil, fmt.Errorf("albumsByArtist %q: %v", name, err)
+	}
+	defer rows.Close()
+
+	// Loop through rows, using Scan to assign column data to struct fields.
+	for rows.Next() {
+		var alb Album
+		if err := rows.Scan(&alb.ID, &alb.Title, &alb.Artist, &alb.Price); err != nil {
+			return nil, fmt.Errorf("albumsByArtist %q: %v", name, err)
+		}
+		albums = append(albums, alb)
+	}
+	if err := rows.Err(); err != nil {
+		return nil, fmt.Errorf("albumsByArtist %q: %v", name, err)
+	}
+	return albums, nil
 }
